12aa1d11b30c20c22e14ffb125c38f8d4bbfc67d
ShieldTanker <eogus2346@naver.com>
2025-01-23 15:52:25 +0900

    무한 맵생성, 자동차 컨트롤러, 게임매니저 수정

---
+++ Assets/Scripts/MoveButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButton : MonoBehaviour
+{
+    private bool _isDown;
+    public delegate void MovebuttonDelegate();
+    public event MovebuttonDelegate OnMoveButtondown;
+
+    private void Update()
+    {
+        if (_isDown)
+        {
+            OnMoveButtondown?.Invoke();
+        }
+    }
+
+    public void ButtonDown()
+    {
+        _isDown = true;
+    }
+
+    public void ButtonUp()
+    {
+        _isDown=false;
+    }
+}
